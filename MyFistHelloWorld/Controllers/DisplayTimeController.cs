3e6fd6ef8afea205318c2a29f89a45f9e40fa4fe
Jonathan <jonablink-182@hotmail.com>
2016-01-22 12:51:04 -0500

    Carga Inicial

---
+++ MyFistHelloWorld/Controllers/DisplayTimeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFistHelloWorld.Controllers
+{
+    public class DisplayTimeController : Controller
+    {
+        // GET: DisplayTime
+        public ActionResult Index()
+        {
+            ViewData["CurrentTime"] = DateTime.Now.ToString(CultureInfo.CurrentCulture);
+            return View();
+        }
+    }
+}
