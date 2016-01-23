ffc654b6f4c818c94fd630a60099b45d9e4f4bbe
Jonathan <jonablink-182@hotmail.com>
2016-01-22 22:57:39 -0500

    Dia 1

---
+++ MyFistHelloWorld/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyFistHelloWorld.Models;
+
+namespace MyFistHelloWorld.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult DisplayCustomer(Customer objCustomer)
+        {
+            //Parte inicial de pruebas
+            //Customer objCustomer = new Customer();
+            //objCustomer.Code = "0010";
+            //objCustomer.Name = "Ronal Mamani";
+            //objCustomer.Amount = 34;
+
+            //Segunda parte de pruebas
+            //Customer objCustomer = new Customer();
+            //objCustomer.Code = Request.Form["customercod"].ToString();
+            //objCustomer.Name = Request.Form["customerman"].ToString();
+            //objCustomer.Amount = Convert.ToInt32( Request.Form["customeramo"]);
+
+            //return View("DisplayCustomer",objCustomer );
+            return View(objCustomer);
+        }
+
+        public ActionResult FillCustomer()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyFistHelloWorld/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFistHelloWorld.Models
+{
+    public class Customer
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int Amount { get; set; }
+    }
+}
